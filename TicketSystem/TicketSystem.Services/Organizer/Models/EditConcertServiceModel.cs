5cf5c00aef7dc25122c4b1a37f5f8221b5ed90b7
RumenKeranchev <devtestmail69@gmail.com>
2017-12-07 18:01:33 +0200

    Added Bins And Objs

---
+++ TicketSystem/TicketSystem.Services/Organizer/Models/EditConcertServiceModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TicketSystem.Common.Mapping;
+using TicketSystem.Data.Models;
+
+namespace TicketSystem.Services.Organizer.Models
+{
+    public class EditConcertServiceModel : IMapFrom< Concert >
+    {
+        public string Name { get; set; }
+
+        [ Required ]
+        [ Display( Name = "Start Date" ) ]
+        public DateTime StartDate { get; set; }
+
+        [ Required ]
+        [ Display( Name = "End Date" ) ]
+        public DateTime EndDate { get; set; }
+
+        [ Required ]
+        [ Display( Name = "Max Number of Tickets" ) ]
+        [ Range( 0, int.MaxValue ) ]
+        public int MaxNumberOfTickets { get; set; }
+    }
+}
